de0853976d005d7a1b05d93e4fc8e7886e259f2f
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-06-30 15:42:17 +0300

    Initial Commit

---
+++ Sorting/Sorting.h
@@ -0,0 +1,19 @@
+//
+//  Sorting.h
+//  Sorting
+//
+//  Created by Gulya Boiko on 6/30/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sorting.
+FOUNDATION_EXPORT double SortingVersionNumber;
+
+//! Project version string for Sorting.
+FOUNDATION_EXPORT const unsigned char SortingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sorting/PublicHeader.h>
+
+
